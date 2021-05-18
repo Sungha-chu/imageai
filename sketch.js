65153c14c895149fa04977cb70e66fa2f82ab0c0
Sungha-chu <58934772+Sungha-chu@users.noreply.github.com>
2021-05-18 18:23:48 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = ' https://sungha-chu.github.io/imageai/';
+let imageModelURL = 'https://sungha-chu.github.io/imageai/';
 
 // Video
 let video;
