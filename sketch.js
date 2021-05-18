7480a38da7d808d37a01cbbe3fa4245ae11f6c41
Sungha-chu <58934772+Sungha-chu@users.noreply.github.com>
2021-05-18 22:30:46 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -28,7 +28,30 @@ function preload() {
 function setup() {
   createCanvas(320, 320);
   // Create the video
-  video = createCapture(VIDEO);
+  var constraints = {
+
+    audio: false,
+
+    video: {
+
+      facingMode: {
+
+        exact: "environment"
+
+      }
+
+    }   
+
+    //video: {
+
+      //facingMode: "user"
+
+    //}
+
+  };
+  
+  
+  video = createCapture(constraints);
   video.size(320, 320);
   video.hide();
 
